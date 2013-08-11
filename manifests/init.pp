8cfb9366dd596badf75f28018fdc52f9ffe127fd
Bryan Davis <bd808@bd808.com>
2013-08-11 13:46:26 -0600

    Fix lint error.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,7 @@
 # This is a placeholder class.
 class geektool {
   package { 'geektool':
-    ensure => installed,
+    ensure   => installed,
     source   => 'http://download.tynsoe.org/GeekTool-3.1.1-311.zip',
     provider => compressed_app,
   }
